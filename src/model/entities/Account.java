779931e65a2c5e98d1dc652f729d4f700d0f2903
Matheus Arruda~ <matheussoaresarruda5@gmail.com>
2022-11-23 18:59:40 -0300

    bank program version 1

---
+++ src/model/entities/Account.java
@@ -0,0 +1,64 @@
+package model.entities;
+
+import model.exceptions.DomainExceptions;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(Double amount) {
+		if(amount > withdrawLimit) {
+			throw new DomainExceptions("The amount exceeds withdraw limit");
+		}
+		if(amount > balance) {
+			throw new DomainExceptions("Not enough balance");
+		}
+		balance -= amount;
+	}
+	
+}
